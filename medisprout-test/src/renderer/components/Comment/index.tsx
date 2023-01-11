fabcc24f625266bcaae5b399d9e6e25eeb001107
lautaro <lauta_3011@hotmail.com>
2023-01-11 20:41:21 -0300

    sliced strings to show desired length

--- medisprout-test/src/renderer/components/Comment/index.tsx
+++ medisprout-test/src/renderer/components/Comment/index.tsx
@@ -8,8 +8,8 @@ const Comment = (props: TComment): JSX.Element => {
     return (
         <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 }, backgroundColor: 'primary.dark', '&:hover': { backgroundColor: 'primary.main' }}} onClick={() => navigate(`/comment/${props.id}`)} key={props.id} >
             <TableCell component="th" scope="row">{props.email}</TableCell>
-            <TableCell align="right">{props.name}</TableCell>
-            <TableCell align="right">{props.body}</TableCell>
+            <TableCell align="right">{props.name.length >= 20 ? props.name.slice(0, 20) + '...' : props.name}</TableCell>
+            <TableCell align="right">{props.body.length >= 20 ? props.body.slice(0, 30) + '...' : props.body}</TableCell>
         </TableRow>
     )
 }
